0526fb90edc239e00e20d25d9825c3988dec5e9f
陈亮 <PJLAB\chenliang@pjnl104220095l.pjlab.org>
2023-12-18 15:30:30 +0800

    add login router

--- co_editor_frontend/src/App.svelte
+++ co_editor_frontend/src/App.svelte
@@ -1,85 +1,17 @@
 <script>
-  import { onMount } from 'svelte';
-
-  import * as Y from 'yjs';
-  import { WebrtcProvider } from 'y-webrtc';
-  import { QuillBinding } from 'y-quill';
-  import Quill from 'quill';
-  import QuillCursors from 'quill-cursors';
-  import 'quill/dist/quill.snow.css';
-
-  // 使用 cursors 插件
-  Quill.register('modules/cursors', QuillCursors);
-
-  let quill;
-
-  async function sendPostRequest(data) {
-    try {
-      console.log(data);
-      const response = await fetch('http://127.0.0.1:4000', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        console.log('POST request successful');
-      } else {
-        console.log('POST request failed');
-      }
-    } catch (error) {
-      console.error('Error sending POST request:', error);
-    }
-  }
-
-  onMount(() => {
-    const ydoc = new Y.Doc();
-    const provider = new WebrtcProvider('co_editor', ydoc);
-    const type = ydoc.getText('quill');
-
-    // @ts-ignore
-    quill = new Quill(document.querySelector('#app'), {
-      modules: {
-        cursors: true,
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          ['image', 'code-block'],
-        ],
-        history: {
-          userOnly: true, // 用户自己实现历史记录
-        },
-      },
-      placeholder: 'input...',
-      theme: 'snow',
-    });
-
-    const binding = new QuillBinding(type, quill, provider.awareness);
-
-    // All of our network providers implement the awareness crdt
-    const awareness = provider.awareness
-
-    // You can observe when a user updates their awareness information
-    awareness.on('change', changes => {
-      // Whenever somebody updates their awareness information,
-      // we log all awareness information from all users.
-      const states = Array.from(awareness.getStates().values());
-      sendPostRequest(quill.getText());
-    })
-
-    // You can think of your own awareness information as a key-value store.
-    // We update our "user" field to propagate relevant user information.
-    awareness.setLocalStateField('user', {
-      // Define a print name that should be displayed
-      name: 'Emmanuelle Charpentier',
-      // Define a color that should be associated to the user:
-      color: '#ffb61e' // should be a hex color
-    })
-  });
+    import Router  from 'svelte-spa-router'
+    import LoginIn from "./routes/LoginIn.svelte";
   
-</script>
+    // 路由配置
+    export const routes = {
+      "/": LoginIn,
+      "/login": LoginIn,
+      // 添加其他路由配置
+    };
+  </script>
+  
+<main>
+    <Router {routes} />
+</main>
 
-<div id="app"></div>
-
+  <div id="app"></div>
---
+++ co_editor_frontend/src/routes/LoginIn.svelte
@@ -0,0 +1,56 @@
+<script>
+    let username = "";
+    let password = "";
+
+    function handleLogin() {
+        // mock返回结果
+        console.log("mock");
+    }
+</script>
+
+<main>
+    <h1>Login Page</h1>
+
+    <form on:submit|preventDefault={handleLogin}>
+        <label>
+            Username:
+            <input type="text" bind:value={username} />
+        </label>
+
+        <label>
+            Password:
+            <input type="password" bind:value={password} />
+        </label>
+
+        <button type="submit">Login</button>
+    </form>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    label {
+        display: flex;
+        flex-direction: column;
+    }
+
+    input {
+        padding: 0.5rem;
+    }
+
+    button {
+        padding: 0.5rem 1rem;
+    }
+</style>
